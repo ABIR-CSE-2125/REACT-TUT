91c30bfe80e38f5e8f96851533116e2f27b86607
Abir Bera <abirbera2003@gmail.com>
2024-04-09 22:03:22 +0530

    undersatding underlying arch & rendering chai

--- 01viteReact/src/App.jsx
+++ 01viteReact/src/App.jsx
@@ -1,5 +1,14 @@
+import Chai from "./chai";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    //<></> ==> are called fragment. As in a jsx file only one tag can be returned so we need to wrap more than oe tag in a fragment;
+
+    <>
+      <h1>This is a learning from chai aur code</h1>
+      <Chai />
+    </>
+  );
 }
 
 export default App;
---
+++ 01viteReact/src/chai.jsx
@@ -0,0 +1,9 @@
+/* 
+the first letter of the function name has to be upppercasae;
+and when working with vite always use jsx files not js files.
+*/
+function Chai() {
+  return <h2>Aharer cha is bish</h2>;
+}
+
+export default Chai;
