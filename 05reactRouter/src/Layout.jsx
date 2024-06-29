9e8c7396b96ae144d424a200a43f93522284d05f
Abir Bera <abirbera2003@gmail.com>
2024-06-29 12:40:11 +0530

    Router upto Contact

---
+++ 05reactRouter/src/Layout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { Footer, Header } from "./components";
+
+export default function Layout() {
+  return (<>
+    <Header />
+    <Outlet />
+    <Footer />
+  </>)
+}
